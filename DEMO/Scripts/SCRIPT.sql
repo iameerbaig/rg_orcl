2b4133d36c9fd7225eb8ee235ed8c065422777d8
Ameer <Ameer.hyder@kumaran.in>
2017-06-14 19:15:16 +0530

    new col added

---
+++ DEMO/Scripts/SCRIPT.sql
@@ -0,0 +1,13 @@
+INSERT INTO employees VALUES 
+        ( 207
+        , 'William A'
+        , 'Gietz'
+        , 'WGIETZ'
+        , '515.123.8181'
+        , TO_DATE('07-06-2002', 'dd-MM-yyyy')
+        , 'AC_ACCOUNT'
+        , 8300
+        , NULL
+        , 205
+        , 110
+        );
--- DEMO/Tables/REGIONS.sql
+++ DEMO/Tables/REGIONS.sql
@@ -1,5 +1,6 @@
 CREATE TABLE demo.regions (
   region_id NUMBER NOT NULL CONSTRAINT region_id_nn CHECK ("REGION_ID" IS NOT NULL),
   region_name VARCHAR2(25 BYTE),
+  Description VARCHAR2(25 BYTE),
   CONSTRAINT reg_id_pk PRIMARY KEY (region_id)
 );
